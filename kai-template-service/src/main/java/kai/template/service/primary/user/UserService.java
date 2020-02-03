76d62967c4fc291e561315121802761fabcf23a3
Kai <610288425@qq.com>
2020-02-03 19:07:36 +0800

    annotation update

--- kai-template-service/src/main/java/kai/template/service/primary/user/UserService.java
+++ kai-template-service/src/main/java/kai/template/service/primary/user/UserService.java
@@ -4,5 +4,15 @@ package kai.template.service.primary.user;
 import kai.template.dto.user.UserDto;
 
 public interface UserService {
-    UserDto selectByUserName(String userName);
+
+    /**
+     * 根据名字获取用户基本信息
+     *
+     * @param userName
+     * @return {@link UserDto}
+     * @throws
+     * @author Kai
+     * @date 2020/2/3 6:57 下午
+     */
+     UserDto selectByUserName(String userName);
 }
